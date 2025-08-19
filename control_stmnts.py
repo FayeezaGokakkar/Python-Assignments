39676789513488de371c4372760632c7a56c048f
FayeezaGokakkar <fayeezagokakkar1@gmail.com>
2025-08-19 06:40:54 -0500

    Add files via upload

---
+++ control_stmnts.py
@@ -0,0 +1,36 @@
+#voting task
+'''age=int(input('Enter your age:'))
+if age>=18:
+    print('yes,you can vote!')
+else:
+    print('no, you cant\'t vote')'''
+#calculator
+
+'''x=float(input('Enter the first number:'))
+y=float(input("Enter the second number:"))
+op=input('Enter the operation(+ - * / %):')
+
+if op=='+':
+    print(x+y)
+elif op=='-':
+    print(x-y)
+elif op=='*':
+    print(x*y)
+elif op=='/':
+    print(x/y)
+elif op=='%':
+    print(x%y)
+else:
+    print('Invalid operaion')
+    print('please choose the correct operator')
+print('Thank you!')'''
+
+
+x=float(input('Enter the first number:'))
+if x%2==0:
+     print('Number is even')
+elif x%2==1:
+     print('Number is odd')
+else:
+     print('its a floating point number')
+
